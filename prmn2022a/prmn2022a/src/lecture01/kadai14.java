af5dc923c7dbb0099338c9800bec6d31326f7363
nekoduka25 <b2211550@photon.chitose.ac.jp>
2022-10-31 18:17:01 +0900

    課題1-4

--- prmn2022a/prmn2022a/src/lecture01/kadai14.java
+++ prmn2022a/prmn2022a/src/lecture01/kadai14.java
@@ -4,12 +4,13 @@ public class kadai14 {
     public static void main(String[] args) {
         int sum = 0;
         int [] array = new int [100];
-        for(int i = 0; i <= 99; i++){
+        for(int i = 0; i < 100; i++){
             array[i] = i + 1;
+
         }
-        for(int j = 0; j <= 99; j++){
+        for(int j = 0; j < 100; j++){
             if(j % 2 == 0){
-                sum = sum + array[j];
+                sum = sum + array[j + 1];
             }
 
         }
