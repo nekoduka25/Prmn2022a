1f4fc2eaeb8e3017dab17ca68d55d2fb7f7d72f7
nekoduka25 <b2211550@photon.chitose.ac.jp>
2022-10-31 17:07:12 +0900

    課題1-2

---
+++ prmn2022a/prmn2022a/src/lecture01/kadai12.java
@@ -0,0 +1,8 @@
+package lecture01;
+
+public class kadai12 {
+    public static void main(String[] args) {
+        int studentNumber = 2211550;
+        System.out.println("B" + studentNumber +" Tanaka Daiki");
+    }
+}
