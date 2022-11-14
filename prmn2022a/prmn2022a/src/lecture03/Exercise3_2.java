efed0999b220f8e48397ada441c26015654e329b
nekoduka25 <b2211550@photon.chitose.ac.jp>
2022-11-14 15:05:09 +0900

    課題3-2,4

--- prmn2022a/prmn2022a/src/lecture03/Exercise3_2.java
+++ prmn2022a/prmn2022a/src/lecture03/Exercise3_2.java
@@ -6,15 +6,17 @@ import java.util.Scanner;
 public class Exercise3_2 {
 
     public static void main(String[] args) {
+
         System.out.println("何行分入力しますか？");
         Scanner input = new Scanner(System.in);
-        int line = input.nextInt();
-        ArrayList<Exercise3_2> e = new ArrayList<>();
+        int num = input.nextInt();
 
-        for(int i = 0; i > line; i++){
-            System.out.println("1行目：");
-            int s = input.nextInt();
-            e.add(new Exercise3_2());
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        for(int i = 0; i < num; i++){
+            System.out.println(i + 1 + "行目：");
+            String s = input.next();
+            arrayList.add(s);
 
         }
 
