278849bb72683902996234d9d864fb2255d19251
nekoduka25 <b2211550@photon.chitose.ac.jp>
2022-11-19 23:01:29 +0900

    課題4-1

---
+++ prmn2022a/prmn2022a/src/lecture04/Exercise4_1.java
@@ -0,0 +1,23 @@
+package lecture04;
+
+public class Exercise4_1 {
+    public static void main(String[] args) {
+
+        Fighter fighter1 = new Fighter(210,100,"Fighter1");
+        Fighter fighter2 = new Fighter(180,150,"Fighter2");
+
+        while(true){
+            fighter1.attack(fighter2);
+            if(fighter2.isAlive() == false){
+                System.out.println(fighter2.getName() + "は倒れた");
+                break;
+            }
+            fighter2.attack(fighter1);
+            if(fighter2.isAlive() == false){
+                System.out.println(fighter1.getName() + "は倒れた");
+                break;
+            }
+
+        }
+    }
+}
---
+++ prmn2022a/prmn2022a/src/lecture04/Fighter.java
@@ -0,0 +1,40 @@
+package lecture04;
+
+public class Fighter {
+
+    private int hitPoint;
+    private int Power;
+    private String name;
+
+    public Fighter(int hitPoint, int Power, String name) {
+        this.hitPoint = hitPoint;
+        this.Power = Power;
+        this.name = name;
+    }
+
+    public void attack(Fighter enemy) {
+        enemy.hitPoint = enemy.hitPoint - this.Power;
+        System.out.println(this.name + "は" + enemy.name + "に" + this.Power + "ダメージを与えた");
+        System.out.println(enemy.hitPoint);
+
+    }
+
+    public boolean isAlive() {
+        if (this.hitPoint > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public int getHitPoint() {
+        return hitPoint;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+
+
+}
